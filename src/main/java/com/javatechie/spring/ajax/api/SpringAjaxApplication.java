d12ef060bed370ba6a196b29541c44eda353e353
amaltaha67 <amaltaha67@yahoo.com>
2020-08-10 00:20:22 +0300

    Project second commit

--- src/main/java/com/javatechie/spring/ajax/api/SpringAjaxApplication.java
+++ src/main/java/com/javatechie/spring/ajax/api/SpringAjaxApplication.java
@@ -2,9 +2,10 @@ package com.javatechie.spring.ajax.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringAjaxApplication {
+public class SpringAjaxApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringAjaxApplication.class, args);
